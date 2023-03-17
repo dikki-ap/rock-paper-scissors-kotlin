0fa009591305e066030a1d54bf3ac44e15b2968b
Dikki AP <dikki.1710@gmail.com>
2023-03-17 22:40:01 +0700

    Draw & Win Function

--- app/src/main/java/dikki/rockpaperscissors/Game.kt
+++ app/src/main/java/dikki/rockpaperscissors/Game.kt
@@ -5,5 +5,18 @@ import kotlin.random.Random
 object Game {
     private val options = listOf("ROCK", "PAPER", "SCISSORS")
 
+    private val rules = mapOf(
+        "ROCK-SCISSORS" to true,
+        "ROCK-PAPER" to false,
+        "PAPER-ROCK" to true,
+        "PAPER-SCISSORS" to false,
+        "SCISSORS-ROCK" to false,
+        "SCISSORS-PAPER" to true
+    )
+
     fun pickRandomOption() : String = options[Random.nextInt(0,3 )]
+
+    fun isWin(from: String, to: String) : Boolean = rules["$from-$to"]!!
+
+    fun isDraw(from: String, to: String) : Boolean = from == to
 }
