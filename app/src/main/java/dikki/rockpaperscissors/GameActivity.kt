95c52249d1a17aceca0a8cde154bfa5edc29163b
Dikki AP <dikki.1710@gmail.com>
2023-03-17 23:06:24 +0700

    Making GameActivity

---
+++ app/src/main/java/dikki/rockpaperscissors/GameActivity.kt
@@ -0,0 +1,11 @@
+package dikki.rockpaperscissors
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
