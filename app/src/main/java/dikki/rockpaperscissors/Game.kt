fc804e7f2c74355ca422e4e64c992c35d558f211
Dikki AP <dikki.1710@gmail.com>
2023-03-17 22:46:58 +0700

    Creating Unit Test

--- app/src/main/java/dikki/rockpaperscissors/Game.kt
+++ app/src/main/java/dikki/rockpaperscissors/Game.kt
@@ -20,11 +20,15 @@ object Game {
         "SCISSORS" to R.drawable.scissors
     )
 
+    // First one is pickRandomOption, so Computer will pick Random Option
     fun pickRandomOption() : String = options[Random.nextInt(0,3 )]
 
+    // And then display the drawable depends from the Option
     fun pickOptionDrawable(option: String) : Int = optionDrawable[option]!!
 
+    // When user clicks option it will be does the same, and then check if it's win or not
     fun isWin(from: String, to: String) : Boolean = rules["$from-$to"]!!
 
+    // Or maybe "draw"
     fun isDraw(from: String, to: String) : Boolean = from == to
 }
---
+++ app/src/test/java/dikki/rockpaperscissors/GameTest.kt
@@ -0,0 +1,4 @@
+package dikki.rockpaperscissors
+
+class GameTest {
+}
