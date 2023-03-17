3c0d69ad89657441032d4deab1abf6783a0e7bfc
Dikki AP <dikki.1710@gmail.com>
2023-03-17 22:55:51 +0700

    Adding pickRandomOption Test

--- app/src/test/java/dikki/rockpaperscissors/GameTest.kt
+++ app/src/test/java/dikki/rockpaperscissors/GameTest.kt
@@ -1,4 +1,14 @@
 package dikki.rockpaperscissors
 
+import org.junit.Assert
+import org.junit.Test
+
 class GameTest {
+    @Test
+    fun testPickRandomOption() {
+        val options = listOf("ROCK", "PAPER", "SCISSORS")
+        Assert.assertTrue(options.contains(Game.pickRandomOption()))
+        Assert.assertTrue(options.contains(Game.pickRandomOption()))
+        Assert.assertTrue(options.contains(Game.pickRandomOption()))
+    }
 }
