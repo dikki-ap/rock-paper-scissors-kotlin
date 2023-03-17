9533436068a807c2ed11cf5689fb7f9be81774bb
Dikki AP <dikki.1710@gmail.com>
2023-03-17 22:59:39 +0700

    Adding pickDrawable Test

--- app/src/test/java/dikki/rockpaperscissors/GameTest.kt
+++ app/src/test/java/dikki/rockpaperscissors/GameTest.kt
@@ -5,10 +5,19 @@ import org.junit.Test
 
 class GameTest {
     @Test
+    // Testing for pickRandomOption
     fun testPickRandomOption() {
         val options = listOf("ROCK", "PAPER", "SCISSORS")
         Assert.assertTrue(options.contains(Game.pickRandomOption()))
         Assert.assertTrue(options.contains(Game.pickRandomOption()))
         Assert.assertTrue(options.contains(Game.pickRandomOption()))
+    }
+
+    @Test
+    // Testing for pickOptionDrawable
+    fun testPickDrawable() {
+        Assert.assertEquals(R.drawable.rock, Game.pickOptionDrawable("ROCK"))
+        Assert.assertEquals(R.drawable.paper, Game.pickOptionDrawable("PAPER"))
+        Assert.assertEquals(R.drawable.scissors, Game.pickOptionDrawable("SCISSORS"))
     }
 }
