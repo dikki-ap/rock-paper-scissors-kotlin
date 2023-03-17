8124c21325e0fec72dbde3bd3a1479f168e3a754
Dikki AP <dikki.1710@gmail.com>
2023-03-17 23:31:23 +0700

    Implement logic and layout into activity

--- app/src/main/java/dikki/rockpaperscissors/GameActivity.kt
+++ app/src/main/java/dikki/rockpaperscissors/GameActivity.kt
@@ -2,10 +2,53 @@ package dikki.rockpaperscissors
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
+
+    lateinit var computerImageView: ImageView
+    lateinit var rockImageButton: ImageButton
+    lateinit var paperImageButton: ImageButton
+    lateinit var scissorsImageButton: ImageButton
+    lateinit var resultImageView: ImageView
+
+    private fun initComponents(){
+        computerImageView = findViewById(R.id.computerImageView)
+        rockImageButton = findViewById(R.id.rockImageButton)
+        paperImageButton = findViewById(R.id.paperImageButton)
+        scissorsImageButton = findViewById(R.id.scissorsImageButton)
+        resultImageView = findViewById(R.id.resultImageView)
+    }
+
+    // If u click button, it will be launch startGame() Function
+    // Which is the Function need option as Parameter, and the option will be random
+    private fun initListeners(){
+        rockImageButton.setOnClickListener { startGame("ROCK") }
+        paperImageButton.setOnClickListener { startGame("PAPER") }
+        scissorsImageButton.setOnClickListener { startGame("SCISSORS") }
+    }
+
+    // When option already picked random, it will be set Image from drawable
+    private fun startGame(option: String){
+        val computerOption = Game.pickRandomOption()
+        computerImageView.setImageResource(Game.pickOptionDrawable(computerOption))
+
+        // And checking the condition with when
+        when{
+            // Compare the condition with "option" and "computerOption"
+            Game.isDraw(option, computerOption) -> resultImageView.setImageResource(R.drawable.draw)
+            Game.isWin(option, computerOption) -> resultImageView.setImageResource(R.drawable.win)
+            else -> resultImageView.setImageResource(R.drawable.lose)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.game)
+
+        initComponents()
+        initListeners()
     }
 }
